69cf69d438da7c86fba4fb33b25ab37380281e77
DS-jr <dmitry.k.sokolov@gmail.com>
2023-08-29 12:48:50 +0400

    added two draft variatnts of JS solutions

--- JS_v2_parser_script.js
+++ JS_v2_parser_script.js
@@ -16,6 +16,99 @@ console.log(final_str);
 
 
 
+
+// (task) "create javascript code to parse data from a webpage and put this data automatically to my GoogleSheet spreadsheet"
+
+
+// (CDL)  (v1) From GPT3.5: 
+
+// npm install axios googleapis   // In the repo via Terminal
+
+// const axios = require('axios');
+// const { google } = require('googleapis');
+
+// // Google Sheets API credentials
+// const credentials = require('path_to_your_credentials_file.json');
+// const spreadsheetId = 'your_spreadsheet_id';
+// const range = 'Sheet1!A1'; // Change this to your desired range
+
+// // Webpage URL to fetch data from
+// const webpageUrl = 'https://example.com';
+
+// // Axios request to fetch data from the webpage
+// axios.get(webpageUrl)
+//   .then(response => {
+//     const dataToPutInSheet = response.data; // Modify this based on your parsing logic
+
+//     // Authorize and set up Google Sheets API
+//     const auth = new google.auth.GoogleAuth({
+//       credentials,
+//       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+//     });
+
+//     const sheets = google.sheets({ version: 'v4', auth });
+
+//     // Update the Google Sheet with the fetched data
+//     sheets.spreadsheets.values.update({
+//       spreadsheetId,
+//       range,
+//       valueInputOption: 'RAW',
+//       resource: {
+//         values: [[dataToPutInSheet]],
+//       },
+//     })
+//     .then(() => {
+//       console.log('Data updated successfully in Google Sheet.');
+//     })
+//     .catch(error => {
+//       console.error('Error updating Google Sheet:', error);
+//     });
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data from the webpage:', error);
+//   });
+
+
+
+
+// (CDL)  (v2) From Bard: 
+
+// In the repo via Terminal: 
+// npm install fetch
+// npm install @google/spreadsheet 
+
+// import fetch from "fetch";
+// import { SpreadsheetApp } from "@google/spreadsheet";
+
+// function parseData(url) {
+//   // Get the data from the webpage.
+//   var data = fetch(url).then(response => response.text());
+
+//   // Parse the data into a JavaScript object.
+//   var parsedData = JSON.parse(data);
+
+//   // Write the data to the Google Sheet.
+//   var spreadsheet = SpreadsheetApp.openById("YOUR_SPREADSHEET_ID");
+//   var sheet = spreadsheet.getActiveSheet();
+
+//   // Loop through the data and add each row to the sheet.
+//   for (var i in parsedData) {
+//     sheet.appendRow(parsedData[i]);
+//   }
+// }
+
+// // Set the URL of the webpage that contains the data.
+// var url = "https://www.example.com/data.json";
+
+// // Parse the data and put it in the Google Sheet.
+// parseData(url);
+
+
+
+
+
+
+
 // const arr2 = []; 
 // const arr3 = []; 
 
